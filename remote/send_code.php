a8d43a9a8ce1bb21bc2cd9670f9ddf4ef2ca4133
phamcuong8868 <phamcuong8868@gmail.com>
2025-09-17 18:56:11 +0700

    editing

--- remote/send_code.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include ($_SERVER['DOCUMENT_ROOT'].'/music-player/ketnoidb.php');
-	include($_SERVER['DOCUMENT_ROOT'] . '/music-player/security/onlyDomain.php');
-	$data = json_decode(file_get_contents('php://input'),true);
-	$sql1 = "UPDATE remote SET malenh='" . $data['maLenh'] . "',danhan=0 WHERE pageId=" . $data['pageId'];
-	$query1 = mysqli_query($db,$sql1);
-?>
--- security/onlyDomain.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
-    $allowed_host = 'https://mlnd.pvcgo.net';
-    if (strpos($referer, $allowed_host) !== 0) 
-    {
-        header('HTTP/1.1 403 Forbidden');
-        die('Access denied.');
-    }
-?>
